9599b9e2f7d9dec91e9b2655197e0c02b1640d72
Shuting <shutingli8999@gmail.com>
2025-05-08 17:39:04 +0800

    init

---
+++ app/src/main/java/com/example/thisensorapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.thisensorapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // hihihi
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
